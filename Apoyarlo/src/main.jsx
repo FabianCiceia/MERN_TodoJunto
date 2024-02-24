e9364026eac1cb2e53801d2b3f73da4e3a47e22c
FabianCiceia <ciceiafabian@gmail.com>
2024-02-24 01:57:36 -0300

    Resolviendo tarea de "Apoyarlo"

---
+++ Apoyarlo/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PersonCard from './components/PersonCard.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PersonCard
+      firstName = "Doe"
+      lastName = "Jane"
+      age = "45"
+      hairColor ="Black"
+    />
+        <PersonCard
+      firstName = "Smith"
+      lastName = "John"
+      age = "88"
+      hairColor ="Brown"
+    />
+        <PersonCard
+      firstName = "Fillmore"
+      lastName = "Millard"
+      age = "50"
+      hairColor ="Brown"
+    />
+        <PersonCard
+      firstName = "Smith"
+      lastName = "Maria"
+      age = "62"
+      hairColor ="Brown"
+    />
+  </React.StrictMode>,
+)
